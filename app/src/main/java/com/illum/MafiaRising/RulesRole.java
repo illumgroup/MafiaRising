f862df078dba56c60f91d601f287c2fdbf2a919e
chelkyl <chelkyl@g.clemson.edu>
2016-12-30 00:52:53 -0500

    Changed rules roles menu screen to generate buttons dynamically from string array Implemented dynamic rules roles card screens Added all rules roles screens

---
+++ app/src/main/java/com/illum/MafiaRising/RulesRole.java
@@ -0,0 +1,49 @@
+package com.illum.MafiaRising;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RulesRole extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_rules_role);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+
+        String[] rolesNames = getResources().getStringArray(R.array.roles_names);
+        String[] rolesStances = getResources().getStringArray(R.array.roles_stances);
+        String[] rolesImgs = getResources().getStringArray(R.array.roles_imgs);
+        String[] rolesInfo = getResources().getStringArray(R.array.roles_info);
+
+        int i = getIntent().getIntExtra("index",0);
+
+        View header = findViewById(R.id.header);
+        View headerContent = header.findViewById(R.id.header_content);
+        CustomFontTextView title = (CustomFontTextView) headerContent.findViewById(R.id.title);
+        title.setText(rolesNames[i]);
+        CustomFontTextView titleSub = (CustomFontTextView) headerContent.findViewById(R.id.subtitle);
+        titleSub.setText(rolesStances[i]);
+
+        View content = findViewById(R.id.content);
+        View contentImgHolder = content.findViewById(R.id.content_img);
+        ImageView img = (ImageView) contentImgHolder.findViewById(R.id.role_img);
+        img.setImageDrawable(ContextCompat.getDrawable(this,getResources().getIdentifier(rolesImgs[i],"drawable",this.getPackageName())));
+
+        View contentTextHolder = content.findViewById(R.id.content_text);
+        TextView text = (TextView) contentTextHolder.findViewById(R.id.role_info);
+        text.setText(rolesInfo[i]);
+    }
+}
--- app/src/main/java/com/illum/MafiaRising/RulesRoles.java
+++ app/src/main/java/com/illum/MafiaRising/RulesRoles.java
@@ -3,8 +3,13 @@ package com.illum.MafiaRising;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class RulesRoles extends BaseActivity {
+
+    int rolesLen;
+    String[] rolesNames, rolesStances, rolesInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,34 @@ public class RulesRoles extends BaseActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
+
+        rolesNames = getResources().getStringArray(R.array.roles_names);
+        rolesStances = getResources().getStringArray(R.array.roles_stances);
+        rolesInfo = getResources().getStringArray(R.array.roles_info);
+
+        rolesLen = rolesNames.length;
+        double btn_weight = 1.0/rolesLen;
+
+        ViewGroup content = (ViewGroup) findViewById(R.id.content);
+
+        for(int i=0;i<rolesLen;++i) {
+            getLayoutInflater().inflate(R.layout.fragment_rules_roles_button, content, true);
+            View btnView = content.getChildAt(i);
+            CustomFontButton btn = (CustomFontButton) btnView.findViewById(R.id.btn);
+            btn.setText(rolesNames[i]);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) btnView.getLayoutParams();
+            params.weight = (float)btn_weight;
+            btnView.setLayoutParams(params);
+        }
+
     }
-    // maybe change to use list adapter
+
+    public void onClickRoleBtn(View btn) {
+        View btnHolder = (View) btn.getParent();
+        ViewGroup content = (ViewGroup) btnHolder.getParent();
+        int i = content.indexOfChild(btnHolder);
+        Intent intent = new Intent(this, RulesRole.class);
+        intent.putExtra("index",i);
+        startActivity(intent);
+    }
 }
