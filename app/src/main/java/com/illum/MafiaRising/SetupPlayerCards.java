757734f0d66cb98d55dc9e4cee1ece92b15b6621
chelkyl <chelkyl@g.clemson.edu>
2017-01-01 16:15:46 -0500

    Added some logic and todos to Setup Players, Extra Roles, and Player Cards

--- app/src/main/java/com/illum/MafiaRising/SetupPlayerCards.java
+++ app/src/main/java/com/illum/MafiaRising/SetupPlayerCards.java
@@ -1,12 +1,16 @@
 package com.illum.MafiaRising;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class SetupPlayerCards extends BaseActivity {
 
+    SharedPreferences sharedPrefs;
     private int playerIndex;
     private int playerCount;
 
@@ -15,14 +19,26 @@ public class SetupPlayerCards extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_player_cards);
 
-        playerIndex = getIntent().getIntExtra("playerIndex",1);
-        //TODO: grab number of players from game session pref
-
         init();
+
+        final String sharedPrefsFileKey = getString(R.string.app_package) + "." + getString(R.string.game_session_file_key);
+        sharedPrefs = getSharedPreferences(sharedPrefsFileKey, Context.MODE_PRIVATE);
+
+        final String sharedPrefsPlayerCountkey = getString(R.string.app_package) + "." + getString(R.string.game_session_player_count_key);
+        final String sharedPrefsPlayerCountMin = getString(R.string.game_session_player_count_min);
+        playerCount = sharedPrefs.getInt(sharedPrefsPlayerCountkey, Integer.parseInt(sharedPrefsPlayerCountMin));
+
+        playerIndex = getIntent().getIntExtra("playerIndex", playerCount);
+
+        //TODO: learn how to use camera to take pictures
+        //TODO: store images somewhere secure
+        //TODO: delete previous images if still present (in case game crashed last time)
+        //TODO: on btn press take picture
+        //TODO: after take image, replace placeholder with image or set image as src and placeholder as bkgd and set right arrow to visible and set camera btn to hide
     }
 
     public void onClickNext(View view) {
-        if(playerIndex <= 1) {
+        if(playerIndex >= playerCount-1) {
             Intent intent = new Intent(this, GameReady.class);
             startActivityForResult(intent, BaseActivity.REQUEST_EXIT_CODE);
 
@@ -30,7 +46,7 @@ public class SetupPlayerCards extends BaseActivity {
         }
         else {
             Intent intent = new Intent(this, SetupPlayerCards.class);
-            intent.putExtra("playerIndex", playerIndex - 1);
+            intent.putExtra("playerIndex", playerIndex + 1);
             startActivityForResult(intent, BaseActivity.REQUEST_EXIT_CODE);
         }
     }
--- app/src/main/java/com/illum/MafiaRising/SetupPlayerCount.java
+++ app/src/main/java/com/illum/MafiaRising/SetupPlayerCount.java
@@ -1,10 +1,15 @@
 package com.illum.MafiaRising;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.NumberPicker;
 
 public class SetupPlayerCount extends BaseActivity {
+
+    SharedPreferences sharedPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +17,31 @@ public class SetupPlayerCount extends BaseActivity {
         setContentView(R.layout.activity_setup_player_count);
 
         init();
+
+        final String sharedPrefsFileKey = getString(R.string.app_package) + "." + getString(R.string.game_session_file_key);
+        sharedPrefs = getSharedPreferences(sharedPrefsFileKey, Context.MODE_PRIVATE);
+
+        final String sharedPrefsPlayerCountkey = getString(R.string.app_package) + "." + getString(R.string.game_session_player_count_key);
+        final String sharedPrefsPlayerCountMin = getString(R.string.game_session_player_count_min);
+        final String sharedPrefsPlayerCountMax = getString(R.string.game_session_player_count_max);
+
+        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.numPlayersPicker);
+        numberPicker.setMinValue(Integer.parseInt(sharedPrefsPlayerCountMin));
+        numberPicker.setMaxValue(Integer.parseInt(sharedPrefsPlayerCountMax));
+
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                SharedPreferences.Editor sharedPrefsEditor = sharedPrefs.edit();
+                sharedPrefsEditor.putInt(sharedPrefsPlayerCountkey, newVal);
+                sharedPrefsEditor.apply();
+            }
+        });
+
     }
 
     public void onClickNext(View view) {
         Intent intent = new Intent(this, SetupExtraRoles.class);
-        //TODO: get numberpicker working and replace hard-coded int
-        //TODO: save numPlayers to game session pref
         startActivityForResult(intent, BaseActivity.REQUEST_EXIT_CODE);
     }
 
